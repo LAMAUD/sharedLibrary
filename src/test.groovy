cb58597a65d6894991a09657093ddab0a50ed733
Cédric LAMAUD <c.lamaud@treeptik.fr>
2019-12-11 15:01:13 +0100

    evol

---
+++ src/test.groovy
@@ -0,0 +1,5 @@
+def call(String test) {
+  sh("echo this is my function in Shared Library ${test}")
+}
+
+return this 
--- test.groovy
+++
@@ -1,5 +0,0 @@
-def call(String test) {
-  sh "echo this is my function in Shared Library ${test}"
-}
-
-return this 
